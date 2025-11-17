8bb0e4d885d02ebc1c8037c056fa49e89579338d
Harsh Prajapati <harshprajapati8923@gmail.com>
2025-11-17 22:57:43 +0530

    add code execution file in chap 1

--- Chapter 1/codeExecution.py
+++ Chapter 1/codeExecution.py
@@ -1,3 +1,13 @@
 print("Line 1")
 print("Line 2")
 print("Line 3")
+
+
+#Variable Assignment and Reassignment 
+name='harsh'
+age=21
+age1=22
+print("Actual  Value :", age1)
+favSubject='Python'
+age1=age
+print("changed value :" , age1)
